fd9f4369d5f723cc34e081d8d0b33826cd507edc
Jami Dileep Kumar <dileepkumarjami@gmail.com>
2021-09-06 12:19:56 +0530

    :recycle: feat: integrated sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import './App.scss';
-import Header from "./components/header/Header";
+import React, { useState } from "react";
+import { Layout } from "antd";
+import TopicMenu from "./components/feature-menu/TopicMenu";
+
+import "./App.scss";
+import NavBar from "./components/navbar/Navbar";
+import SideBar from "./components/sidebar/Sidebar";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    const topics = ["Order", "Customer", "Customer Feedback", "Product"];
+    const [contentIndex, setContentIndex] = useState(0);
+    const [selectedKey, setSelectedKey] = useState("0");
+    const changeSelectedKey = (event) => {
+        const key = event.key;
+        setSelectedKey(key);
+        setContentIndex(+key);
+    };
+    const Menu = (
+        <TopicMenu
+            topics={topics}
+            selectedKey={selectedKey}
+            changeSelectedKey={changeSelectedKey}
+        />
+    );
+    return (
+        <div className="App">
+            <NavBar menu={Menu} />
+            <Layout>
+                <SideBar menu={Menu} />
+                <Layout.Content className="content">
+                    {topics[contentIndex]}
+                </Layout.Content>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
