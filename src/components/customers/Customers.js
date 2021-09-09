6b1182853390f2c7b976456d11a32f4398cd4a3e
Jami Dileep Kumar <dileepkumarjami@gmail.com>
2021-09-09 18:24:54 +0530

    :sparkles: feat: added customers component

---
+++ src/components/customers/Customers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Customers.scss';
+const Customers = () => {
+    return (
+        <div>
+            <h1>Customers</h1>
+        </div>
+    )
+}
+export default Customers;
