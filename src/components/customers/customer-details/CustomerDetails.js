b9a644a6422f3fb8b5946661cf259ffa6d3256c3
Jami Dileep Kumar <dileepkumarjami@gmail.com>
2021-09-09 22:31:33 +0530

    :iphone: style: made the customer details content responsive

--- src/components/customers/customer-details/CustomerDetails.js
+++ src/components/customers/customer-details/CustomerDetails.js
@@ -8,32 +8,32 @@ const CustomerDetails = ({match}) => {
             <h1 className='page-title'>Customer</h1>
             <div className='customer-info'>
                 <Row className='bg-white'>
-                    <Col span={6} className='customer-info-label'>Restaurant Name</Col>
-                    <Col span={6}>Sky Deck View Bar</Col>
+                    <Col flex="300px" className='customer-info-label'>Restaurant Name</Col>
+                    <Col flex="auto">Sky Deck View Bar</Col>
                 </Row>
                 <Row>
-                    <Col span={6} className='customer-info-label'>Address</Col>
-                    <Col span={6}>The Bayleaf, Muralla St, Intramuros, Manila, 1002 Metro Manila, Philippines</Col>
+                    <Col flex="300px" className='customer-info-label'>Address</Col>
+                    <Col flex="auto">The Bayleaf, Muralla St, Intramuros, Manila, 1002 Metro Manila, Philippines</Col>
                 </Row>
                 <Row className='bg-white'>
-                    <Col span={6} className='customer-info-label'>Tax Identification Number</Col>
-                    <Col span={6}>7382942</Col>
+                    <Col flex="300px" className='customer-info-label'>Tax Identification Number</Col>
+                    <Col flex="auto">7382942</Col>
                 </Row>
                 <Row>
-                    <Col span={6} className='customer-info-label'>Phone Number</Col>
-                    <Col span={6}>+63 (0)2 318 5000</Col>
+                    <Col flex="300px" className='customer-info-label'>Phone Number</Col>
+                    <Col flex="auto">+63 (0)2 318 5000</Col>
                 </Row>
                 <Row className='bg-white'>
-                    <Col span={6} className='customer-info-label'>Contact Person Name</Col>
-                    <Col span={6}>Dileep Kumar Jami</Col>
+                    <Col flex="300px" className='customer-info-label'>Contact Person Name</Col>
+                    <Col flex="auto">Dileep Kumar Jami</Col>
                 </Row>
                 <Row>
-                    <Col span={6} className='customer-info-label'>Contact Person Email</Col>
-                    <Col span={6}>jami@horizons.ventures</Col>
+                    <Col flex="300px" className='customer-info-label'>Contact Person Email</Col>
+                    <Col flex="auto">jami@horizons.ventures</Col>
                 </Row>
                 <Row className='bg-white'>
-                    <Col span={6} className='customer-info-label'>Contact Person Phone Number</Col>
-                    <Col span={6}>8136889404</Col>
+                    <Col flex="300px" className='customer-info-label'>Contact Person Phone Number</Col>
+                    <Col flex="auto">8136889404</Col>
                 </Row>
             </div>
         </div>
